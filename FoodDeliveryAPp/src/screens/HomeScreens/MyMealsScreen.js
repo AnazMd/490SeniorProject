47afec8f37a55b5c51e2b39555738b40253a5e4a
franciscocedeno35 <francisco.cedeno.35@my.csun.edu>
2023-03-19 16:45:10 -0700

    Added an Activity Indicator to show that the meal page is loading
    It seems that the first time in a day that I open the app, it takes a while to fetch the information from spoonacular. I don't know if it is loading or just stuck cause I close the app and try like another two times before it works. The activity indicator will show that it is loading.

--- FoodDeliveryAPp/src/screens/HomeScreens/MyMealsScreen.js
+++ FoodDeliveryAPp/src/screens/HomeScreens/MyMealsScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import{useState, useEffect} from "react";
-import { View, SafeAreaView, Text, StyleSheet, Dimensions, Button, TextInput, TouchableOpacity, Image} from "react-native";
+import { View, SafeAreaView, Text, StyleSheet, Dimensions, Button, TextInput, TouchableOpacity, Image, ActivityIndicator} from "react-native";
 import { colors, parameters, title } from "../../constants/styles";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { SCREEN_NAMES } from "../../constants/navigation";
@@ -8,11 +8,15 @@ import { Screen } from "../../components/layout/Screen";
 
 export function MyMealsScreen() {
   const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchRecipes = () => {
     fetch(`https://api.spoonacular.com/recipes/findByNutrients?number=3&random=true&minCalories=0&apiKey=ccc7636d8ca643b3aeaa0428a3e1efe9`)
       .then(response => response.json())
-      .then(data => setRecipes(data))
+      .then(data => {
+        setRecipes(data);
+        setIsLoading(false);
+      })
       .catch(error => console.log(error));
   };
 
@@ -21,9 +25,15 @@ export function MyMealsScreen() {
   }, []);
 
   const generateMeals = () => {
+    setIsLoading(true);
     fetchRecipes();
     alert('Generating Meals');
   }
+
+  // Define the colors object
+  const colors = {
+  primary: "006aff",
+  };
 
   return (
     <SafeAreaView style={{
@@ -35,26 +45,36 @@ export function MyMealsScreen() {
     }}>
       <Text style={{ fontSize: 48, fontWeight: 'bold', textAlign: 'center' }}>Daily Meals</Text>
       
-      <View style={{ flexDirection: 'column', justifyContent: 'space-around'}}>
-        {recipes && recipes.map(recipe => (
-          <TouchableOpacity key={recipe.id} style={{ 
-            backgroundColor: "hsl(136, 92%, 55%)",
-            flex: 0.295,
-            aspectRatio: 1.8,
-            marginHorizontal: 8,
-            alignItems: 'center', 
-            justifyContent: 'center',
-            borderRadius: 50,
-          }}>
-              <Image source={{ uri: recipe.image }} style={{ width: 100, height: 100, borderRadius: 10 }} />
-              <Text>{recipe.title}</Text>
-              <Text>Calories: {recipe.calories}kcal</Text>
-              <Text>Fat: {recipe.fat}</Text>
-              <Text>Carbs: {recipe.carbs}</Text>
-              <Text>Protein: {recipe.protein}</Text>
-          </TouchableOpacity>
-        ))}  
-      </View>
+      {isLoading ? (
+        // When loading
+        <View style={{ flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+          <ActivityIndicator size={100} color={colors.primary} />
+          <Text style={{ textAlign: "center" }}>Loading...</Text>
+        </View>
+      ) : (
+        // When loaded
+        <View style={{ flexDirection: 'column', justifyContent: 'space-around'}}>
+          {recipes && recipes.map(recipe => (
+            <TouchableOpacity key={recipe.id} style={{ 
+              backgroundColor: "hsl(136, 92%, 55%)",
+              flex: 0.295,
+              aspectRatio: 1.8,
+              marginHorizontal: 8,
+              alignItems: 'center', 
+              justifyContent: 'center',
+              borderRadius: 50,
+            }}>
+                <Image source={{ uri: recipe.image }} style={{ width: 100, height: 100, borderRadius: 10 }} />
+                <Text>{recipe.title}</Text>
+                <Text>Calories: {recipe.calories}kcal</Text>
+                <Text>Fat: {recipe.fat}</Text>
+                <Text>Carbs: {recipe.carbs}</Text>
+                <Text>Protein: {recipe.protein}</Text>
+            </TouchableOpacity>
+          ))}  
+        </View>
+      )}
+
       <Button style={{width:'50%', height:'10%'}} title="Generate New Meals" color="blue" onPress={generateMeals}></Button>
     </SafeAreaView>
   );
