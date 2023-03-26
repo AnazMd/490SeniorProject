2c9f6c5d102aa0ba3f9d73bf463694986bb90f19
Anaz Mohammed <97768378+AnazMd@users.noreply.github.com>
2023-03-25 17:05:21 -0700

    changed login and signup UI

--- FoodDeliveryAPp/src/screens/HomeScreens/MyMealsScreen.js
+++ FoodDeliveryAPp/src/screens/HomeScreens/MyMealsScreen.js
@@ -1,6 +1,17 @@
 import React from "react";
-import{useState, useEffect} from "react";
-import { View, SafeAreaView, Text, StyleSheet, Dimensions, Button, TextInput, TouchableOpacity, Image, ActivityIndicator} from "react-native";
+import { useState, useEffect } from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+} from "react-native";
 import { colors, parameters, title } from "../../constants/styles";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { SCREEN_NAMES } from "../../constants/navigation";
@@ -11,13 +22,15 @@ export function MyMealsScreen() {
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchRecipes = () => {
-    fetch(`https://api.spoonacular.com/recipes/findByNutrients?number=3&random=true&minCalories=0&apiKey=ccc7636d8ca643b3aeaa0428a3e1efe9`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `https://api.spoonacular.com/recipes/findByNutrients?number=3&random=true&minCalories=0&apiKey=e095e14b3aba4f8a86d65bbbec9d5258`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         setRecipes(data);
         setIsLoading(false);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
@@ -27,55 +40,79 @@ export function MyMealsScreen() {
   const generateMeals = () => {
     setIsLoading(true);
     fetchRecipes();
-    alert('Generating Meals');
-  }
+    alert("Generating Meals");
+  };
 
   // Define the colors object
   const colors = {
-  primary: "006aff",
+    primary: "006aff",
   };
 
   return (
-    <SafeAreaView style={{
-      flex:1, 
-      flexDirection:'column', 
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      paddingHorizontal: 16,
-    }}>
-      <Text style={{ fontSize: 48, fontWeight: 'bold', textAlign: 'center' }}>Daily Meals</Text>
-      
+    <SafeAreaView
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingHorizontal: 16,
+      }}
+    >
+      <Text style={{ fontSize: 48, fontWeight: "bold", textAlign: "center" }}>
+        Daily Meals
+      </Text>
+
       {isLoading ? (
         // When loading
-        <View style={{ flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+        <View
+          style={{
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <ActivityIndicator size={100} color={colors.primary} />
           <Text style={{ textAlign: "center" }}>Loading...</Text>
         </View>
       ) : (
         // When loaded
-        <View style={{ flexDirection: 'column', justifyContent: 'space-around'}}>
-          {recipes && recipes.map(recipe => (
-            <TouchableOpacity key={recipe.id} style={{ 
-              backgroundColor: "hsl(136, 92%, 55%)",
-              flex: 0.295,
-              aspectRatio: 1.8,
-              marginHorizontal: 8,
-              alignItems: 'center', 
-              justifyContent: 'center',
-              borderRadius: 50,
-            }}>
-                <Image source={{ uri: recipe.image }} style={{ width: 100, height: 100, borderRadius: 10 }} />
+        <View
+          style={{ flexDirection: "column", justifyContent: "space-around" }}
+        >
+          {recipes &&
+            recipes.map((recipe) => (
+              <TouchableOpacity
+                key={recipe.id}
+                style={{
+                  backgroundColor: "hsl(136, 92%, 55%)",
+                  flex: 0.295,
+                  aspectRatio: 1.8,
+                  marginHorizontal: 8,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: 50,
+                }}
+              >
+                <Image
+                  source={{ uri: recipe.image }}
+                  style={{ width: 100, height: 100, borderRadius: 10 }}
+                />
                 <Text>{recipe.title}</Text>
                 <Text>Calories: {recipe.calories}kcal</Text>
                 <Text>Fat: {recipe.fat}</Text>
                 <Text>Carbs: {recipe.carbs}</Text>
                 <Text>Protein: {recipe.protein}</Text>
-            </TouchableOpacity>
-          ))}  
+              </TouchableOpacity>
+            ))}
         </View>
       )}
 
-      <Button style={{width:'50%', height:'10%'}} title="Generate New Meals" color="blue" onPress={generateMeals}></Button>
+      <Button
+        style={{ width: "50%", height: "10%" }}
+        title="Generate New Meals"
+        color="blue"
+        onPress={generateMeals}
+      ></Button>
     </SafeAreaView>
   );
 }
--- FoodDeliveryAPp/src/screens/authScreens/SignUpScreen.js
+++ FoodDeliveryAPp/src/screens/authScreens/SignUpScreen.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import { Screen } from "../../components/layout/Screen";
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import { auth } from "../../../firebase";
+import { Icon } from "react-native-elements/dist/icons/Icon";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export function SignUpScreen({navigation}) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export function SignUpScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleSignUp = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in 
+        // Signed in
         const user = userCredential.user;
-        navigation.navigate('FormFillIn', {
-          userid: user.uid
-        })
-        console.log("uid: ", user.uid)
+        navigation.navigate("FormFillIn", {
+          userid: user.uid,
+        });
+        console.log("uid: ", user.uid);
         // ...
       })
       .catch((error) => {
@@ -23,78 +31,149 @@ export function SignUpScreen({navigation}) {
         const errorMessage = error.message;
         // ..
       });
-      
-  }
+  };
   return (
-    <Screen>
-        <View style={styles.content}>
-        <View style = {styles.inputView}>
-            <TextInput
-                style = {styles.TextInput}
-                placeholder="Enter Your Email"
-                placeholderTextColor = "003f5c"
-                value = {email}
-                onChangeText={text => setEmail(text)}
-            />
+    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+      <View style={{ paddingHorizontal: 25 }}>
+        <View style={{ alignItems: "center" }}>
+          <Icon size={150} type="material" name="restaurant" />
         </View>
-        <View style = {styles.inputView}>
-            <TextInput
-                style = {styles.TextInput}
-                placeholder="Enter Your Password"
-                placeholderTextColor = "003f5c"
-                value = {password}
-                onChangeText={text => setPassword(text)}
-                secureTextEntry
+        <Text style={styles.header}>Register</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            marginBottom: 30,
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.alternateLogin, { backgroundColor: "#4267B2" }]}
+          >
+            <Icon
+              iconStyle={{ color: "white" }}
+              type="ionicon"
+              name="logo-facebook"
             />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.alternateLogin, { backgroundColor: "#DB4437" }]}
+          >
+            <Icon
+              iconStyle={{ color: "white" }}
+              type="ionicon"
+              name="logo-google"
+            />
+          </TouchableOpacity>
         </View>
+        <Text
+          style={{
+            textAlign: "center",
+            color: "#666",
+            marginBottom: 30,
+          }}
+        >
+          Or, register with...
+        </Text>
+        <View style={styles.textInput}>
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Enter Your Email"
+            placeholderTextColor="#ccc"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View style={styles.textInput}>
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Enter Your Password"
+            placeholderTextColor="#ccc"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+          />
+        </View>
+
         <TouchableOpacity style={styles.loginBtn} onPress={handleSignUp}>
-            <Text style={styles.loginText}>Sign Up</Text>
+          <Text style={styles.loginText}>Register</Text>
         </TouchableOpacity>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginBottom: 30,
+          }}
+        >
+          <Text>Already registered?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+            <Text style={{ color: "#AD40AF", fontWeight: "700" }}> Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </Screen>
-  )
+    </SafeAreaView>
+  );
+  // return (
+  //   <Screen>
+  //       <View style={styles.content}>
+  //       <View style = {styles.inputView}>
+  //           <TextInput
+  //               style = {styles.TextInput}
+  //               placeholder="Enter Your Email"
+  //               placeholderTextColor = "003f5c"
+  //               value = {email}
+  //               onChangeText={text => setEmail(text)}
+  //           />
+  //       </View>
+  //       <View style = {styles.inputView}>
+  //           <TextInput
+  //               style = {styles.TextInput}
+  //               placeholder="Enter Your Password"
+  //               placeholderTextColor = "003f5c"
+  //               value = {password}
+  //               onChangeText={text => setPassword(text)}
+  //               secureTextEntry
+  //           />
+  //       </View>
+  //       <TouchableOpacity style={styles.loginBtn} onPress={handleSignUp}>
+  //           <Text style={styles.loginText}>Sign Up</Text>
+  //       </TouchableOpacity>
+  //     </View>
+  //   </Screen>
+  // )
 }
 
 const styles = StyleSheet.create({
-    content: {
-      flex: 1,
-      backgroundColor: "#fff",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    TextInput: {
-      height: 50,
-      flex: 1,
-      padding: 10,
-      marginLeft: 10,
-      //backgroundColor: "white",
-    },
-    inputView: {
-      backgroundColor: "white",
-      borderRadius: 30,
-      borderWidth: 1,
-      width: "70%",
-      height: 45,
-      marginBottom: 20,
-      alignItems: "center",
-    },
-    forgot_button: {
-      height: 30,
-      marginBottom: 30,
-    },
-   
-    loginBtn: {
-      width: "80%",
-      borderRadius: 25,
-      height: 50,
-      borderWidth: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 40,
-      backgroundColor: "lightgray",
-    },
-    loginText: {
-      color: "white",
-      fontSize: "20",
-    }
-  });
+  header: {
+    fontSize: 28,
+    fontWeight: "500",
+    color: "#333",
+    marginBottom: 30,
+  },
+  textInput: {
+    flexDirection: "row",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    paddingBottom: 8,
+    marginBottom: 25,
+  },
+  loginBtn: {
+    backgroundColor: "#AD40AF",
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  loginText: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 16,
+    color: "white",
+  },
+  alternateLogin: {
+    borderColor: "#ddd",
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+  },
+});
