c94942ec732de952e6c54bb187a178102008d4a7
Pouria-Salekani <27398502+Pouria-Salekani@users.noreply.github.com>
2023-03-26 18:10:05 -0700

    added a search bar

--- FoodDeliveryAPp/src/screens/HomeScreens/Search.js
+++ FoodDeliveryAPp/src/screens/HomeScreens/Search.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, ScrollView, SafeAreaView} from "react-native";
+import { View, Text, StyleSheet, Button, ScrollView, SafeAreaView, TextInput} from "react-native";
 
 export function Search(){
     return(
-        <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Search Page</Text>
-        </View>
-    )
-}
+
+    //Search bar below
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="start typing here to search"
+      />
+    </View>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    borderRadius: 5,
+    margin: 10,
+    padding: 5,
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 10,
+  },
+});
